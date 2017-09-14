30884497feccc6b91d728098cf155740ea0c454a
Cyrille <cyrille.rieau@gmail.com>
2017-09-14 12:35:54 +0200

    Commit Fomr et Listing

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import {Compteur} from './compteur';
+import {TodoAjax} from './todo-ajax';
+import * as $ from 'jquery';
 
 const compteur = new Compteur();
 const para = document.querySelector('#para');
@@ -8,3 +10,13 @@ bouton.addEventListener('click', function(e) {
     compteur.increment();
     para.textContent = compteur.showCount().toString();
 })
+const todoAjax = new TodoAjax();
+todoAjax.getAllTodos().then(function(response) {
+    console.log(response);
+    let todo = $('#todo');
+    for(let r of response){
+    let li = $('<li>'+r+'</li>');
+   //let li = $('<li>bloup</li>');
+    todo.append(li);
+    }
+});
--- todo.ts
+++ todo.ts
@@ -6,15 +6,15 @@ let todoAjax = new TodoAjax();
 let data = <HTMLInputElement>document.querySelector('#data');
 let liste = document.querySelector('#liste');
 document.querySelector('#submit').addEventListener('click', function() {
-todoAjax.add(data.value);
-todoAjax.list();
+todoAjax.addTodo(data.value);
+todoAjax.getAllTodos();
 let li = document.createElement("li");
 li.textContent = data.value + " ";
 let del = document.createElement("button");
 del.textContent="Delete";
 del.addEventListener('click', function() {
-    todoAjax.delete(li.innerHTML);
-    todoAjax.list();
+    todoAjax.deleteTodo(li.innerHTML);
+    todoAjax.getAllTodos();
 });
 li.appendChild(del);
 liste.appendChild(li);
